3604cc539034e06152ff1780ebbe466b58f9681f
ZT906311 <feanortang@gmail.com>
2024-04-02 12:55:42 -0700

    separate plotting functions

--- mesa/ecospatial/__init__.py
+++ mesa/ecospatial/__init__.py
@@ -1,1 +1,3 @@
 from ._ecospatial import generate_patches, calculate_shannon_entropy, calculate_diversity_index, calculate_MDI, calculate_GDI, calculate_DPI, diversity_heatmap, global_moran, local_moran, spot_cellfreq
+
+from ._pl import plot_cells_patches, create_circos_plot
